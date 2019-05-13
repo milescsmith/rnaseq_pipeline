aeb291c454bf1c618f266a156e34c2dc35ca4df4
Miles Smith <miles-smith@omrf.org>
2019-05-13 21:50:21 +0000

    updated

--- run_script.sh
+++ run_script.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
-snakemake --cluster-config cluster.json \
+snakemake \
+--configfile $HOME/workspace/rnaseq_pipeline/config.yaml \
+--cluster-config $HOME/workspace/rnaseq_pipeline/cluster.json \
 --cluster \
 "sbatch -p {cluster.partition} \
 --cpus-per-task={cluster.cpus-per-task} \
 --mem={cluster.mem} \
 --nodes={cluster.nodes} \
 --export={cluster.path},{cluster.java_opts}"  \
---jobs 4 \
---snakefile paired_end_rnaseq.snakefile \
+--jobs 12 \
+--snakefile $HOME/workspace/rnaseq_pipeline/Snakefile \
 --use-conda \
---shadow-prefix ./ \
 --rerun-incomplete kallisto_with_qc
